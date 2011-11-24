bad0b7852db03343bd354983421aa98734c1d952
spa.simone@gmail.com <spa.simone@gmail.com@b503de64-eac3-2df9-a2ed-aa921a81908e>
2011-11-24 11:36:52 +0000

    The source is changed. Updated the lib, and fixed some bugs.

--- trunk/FirstApp/src/spa/simone/firstapp/client/PageFactory.java
+++ trunk/FirstApp/src/spa/simone/firstapp/client/PageFactory.java
@@ -2,23 +2,7 @@ package spa.simone.firstapp.client;
 
 public abstract class PageFactory {
 
-    private static FirstPage firstPage;
-    private static SecondPage secondPage;
     private static MainPage mainPage;
-
-    public static FirstPage getFirstPage() {
-	if (firstPage == null) {
-	    firstPage = new FirstPage();
-	}
-	return firstPage;
-    }
-
-    public static SecondPage getSecondPage() {
-	if (secondPage == null) {
-	    secondPage = new SecondPage();
-	}
-	return secondPage;
-    }
 
     public static MainPage getMainPage() {
 	if (mainPage == null) {
