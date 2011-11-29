30e2c28cdfb7df306cdd022519098536a63df5e5
spa.simone@gmail.com <spa.simone@gmail.com@b503de64-eac3-2df9-a2ed-aa921a81908e>
2011-11-29 14:11:10 +0000

    Initial commit

---
+++ PhonegapPlugin/src/spa/simone/phonegaplugin/PhonegapPluginActivity.java
@@ -0,0 +1,13 @@
+package spa.simone.phonegaplugin;
+
+import com.phonegap.*;
+import android.os.Bundle;
+
+public class PhonegapPluginActivity extends DroidGap {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+}
